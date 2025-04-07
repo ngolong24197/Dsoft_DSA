0608b2a7db0c1ca30d86579836e795f1cccc0ac1
long <testingemail@gmail.com>
2025-04-07 16:02:20 +0700

    [1.Two_Sum]

---
+++ 1.Two_sum/two_sum.py
@@ -0,0 +1,22 @@
+from two_sum_function import Solution
+
+
+def main():
+    solution = Solution()
+    
+    nums1 = [2,7,11,15]
+    nums2 =[3,2,4]
+    nums3 = [3,3]
+    
+    result1 = solution.twoSum(nums1,9)
+    print(f"Result1: {result1} ")
+    
+    result2 = solution.twoSum(nums2,6)
+    print(f"Result1: {result2} ")
+    
+    
+    result3 = solution.twoSum(nums3,6)
+    print(f"Result1: {result3} ")
+    
+if __name__ == "__main__":
+    main()
