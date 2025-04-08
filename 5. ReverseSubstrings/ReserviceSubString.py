a45f86a7b481420b07adfc4e63d57038422cb060
ngolong24197 <75208505+ngolong24197@users.noreply.github.com>
2025-04-08 16:15:48 +0700

    Update ReserviceSubString.py
    add Comment

--- 5. ReverseSubstrings/ReserviceSubString.py
+++ 5. ReverseSubstrings/ReserviceSubString.py
@@ -1,4 +1,8 @@
 
+#Description: Reverse the strings in each pair of matching parentheses, starting from the innermost one.
+#Input: a string that consists of lower case English letters and brackets.
+#Output: a reversed string 
+
 from ReserviceSubString_func import Solution
 
 def main():
