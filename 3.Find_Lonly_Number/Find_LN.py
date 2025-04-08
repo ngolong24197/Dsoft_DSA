27da93f1bd4125c5f6290799b077e4c9365dbc5b
ngolong24197 <75208505+ngolong24197@users.noreply.github.com>
2025-04-08 16:19:37 +0700

    Update Find_LN.py
    add comment

--- 3.Find_Lonly_Number/Find_LN.py
+++ 3.Find_Lonly_Number/Find_LN.py
@@ -1,3 +1,8 @@
+#Description: Given an integer array nums, return all the lonely numbers in the array. 
+# A lonely number is a number that appears exactly once in the array and does not have any adjacent numbers (i.e., its value minus 1 and its value plus 1) in the array.
+#Input: an integer array nums
+#Ouput: an integer array of lonely numbers
+
 from Find_LN_Func import *
 
 def main():
