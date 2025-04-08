c245128984ff624428dd121286e47e5e4acd758a
Long Ngo <ngolong24197@gmail.com>
2025-04-08 15:32:05 +0700

    [6.Node_Tree] Modify to not use extra library

--- 6.Node_Tree/Node_Tree.py
+++ 6.Node_Tree/Node_Tree.py
@@ -1,1 +1,17 @@
 
+
+from Node_Tree_func import Solution
+
+def main():
+    solution = Solution()
+
+    result1 = solution.pathInZigZagTree(14)
+    
+    result2 = solution.pathInZigZagTree2(14)
+    
+    print(result1)
+    
+    print(result2)
+    
+if __name__== "__main__":
+    main()
--- 6.Node_Tree/Node_Tree_func.py
+++ 6.Node_Tree/Node_Tree_func.py
@@ -6,6 +6,16 @@ class Solution(object):
         path = []
         while label > 0:
             path.append(label)
-            level = int(math.log2(  ))
+            level = int(math.log2(label))
             label = (2 ** level + 2 ** (level + 1) - 1 - label) // 2
+        return path[::-1]
+  
+  
+    
+    def pathInZigZagTree2(self, label:int):
+        path = []
+        while label > 0:
+            path.append(label)
+            level = label.bit_length()
+            label = (2 ** (level-1) + 2 ** (level) - 1 - label) // 2
         return path[::-1]
