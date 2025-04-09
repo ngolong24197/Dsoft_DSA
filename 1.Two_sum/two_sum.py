93e808b37e55af431b2553ee7cebc216e7a3ac2f
Long Ngo <ngolong24197@gmail.com>
2025-04-09 08:38:32 +0700

    Optimize function using dict

--- 1.Two_sum/two_sum.py
+++ 1.Two_sum/two_sum.py
@@ -3,6 +3,7 @@ from two_sum_function import Solution
 
 def main():
     solution = Solution()
+    solution2 = Solution()
     
     nums1 = [2,7,11,15]
     nums2 =[3,2,4]
@@ -12,11 +13,18 @@ def main():
     print(f"Result1: {result1} ")
     
     result2 = solution.twoSum(nums2,6)
-    print(f"Result1: {result2} ")
+    print(f"Result2: {result2} ")
     
     
     result3 = solution.twoSum(nums3,6)
-    print(f"Result1: {result3} ")
+    print(f"Result4: {result3} ")
+    
+       
+    result4 = solution.twoSum2(nums1,9)
+    print(f"Result4: {result4} ")
+    
+    result5 = solution.twoSum2(nums2,6)
+    print(f"Result5: {result5} ")
     
 if __name__ == "__main__":
     main()
--- 1.Two_sum/two_sum_function.py
+++ 1.Two_sum/two_sum_function.py
@@ -1,8 +1,24 @@
 class Solution(object):
+    
+    
+    ##Use nested loop
     def twoSum(self, nums, target):
         for i in range(len(nums)):
             complement = target - nums[i]
             for j in range(i+1, len(nums)):
                 if nums[j] == complement:
                     return [i,j]
+        return []
+    
+    ##Use dictionary
+    def twoSum2(self, nums, target):
+        num_to_index = {}
+        for i,num in enumerate(nums):
+            complement = target - num
+            
+            if complement in num_to_index:
+                return [num_to_index[complement],i]
+            
+            num_to_index[num] = i
+            
         return []
